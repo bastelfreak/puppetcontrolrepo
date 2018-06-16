826f4377855d12086c037097b6bf64fd16a09395
Tim Meusel <tim@bastelfreak.de>
2018-06-17 00:31:34 +0200

    fuck you ssl

--- modules/profiles/manifests/puppetdb.pp
+++ modules/profiles/manifests/puppetdb.pp
@@ -10,7 +10,6 @@ class profiles::puppetdb {
     manage_firewall         => false,
     manage_dbserver         => true,
     postgres_version        => '10',
-    ssl_deploy_certs        => true,
     ssl_set_cert_paths      => true,
     disable_update_checking => true,
     ssl_key                 => "/etc/puppetlabs/puppet/ssl/private_keys/${facts['fqdn']}.pem",
