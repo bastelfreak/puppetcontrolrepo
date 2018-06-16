c740b7fed99782693821dcb570ebb656a43cbdd6
Tim Meusel <tim@bastelfreak.de>
2018-06-16 22:15:07 +0200

    fix puppetdb setup

--- modules/profiles/manifests/puppetdb.pp
+++ modules/profiles/manifests/puppetdb.pp
@@ -1,5 +1,12 @@
 class profiles::puppetdb {
-  class { 'puppetdb::server':
+  # setup the database
+  class{'postgresql::globals':
+    encoding            => 'UTF-8',
+    locale              => 'en_US.UTF-8',
+    manage_package_repo => true,
+    version             => '10',
+  }
+  -> class { 'puppetdb':
     #java_args         => {
       #'-Xmx' => '8192m',
       #'-Xms' => '2048m',
