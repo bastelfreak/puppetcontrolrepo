c026f490402bee24e71631742964aa90c8018af5
Tim Meusel <tim@bastelfreak.de>
2018-06-16 19:51:10 +0200

    deploy puppetdb as well

--- modules/roles/manifests/puppetserver.pp
+++ modules/roles/manifests/puppetserver.pp
@@ -1,5 +1,6 @@
 class roles::puppetserver {
   include profiles::puppetserver
+  include profiles::puppetdb
   include profiles::foreman
   Class['Profiles::Puppetserver']
   -> Class['Profiles::Foreman']
