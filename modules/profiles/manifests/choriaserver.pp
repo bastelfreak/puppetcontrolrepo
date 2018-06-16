4fced41efdfbab5949d3fb884c6726ed05c900e1
Tim Meusel <tim@bastelfreak.de>
2018-06-17 01:05:11 +0200

    choria

--- modules/profiles/manifests/choriaserver.pp
+++ modules/profiles/manifests/choriaserver.pp
@@ -2,6 +2,7 @@ class profiles::choriaserver {
   if($facts['os']['name'] == 'CentOS') {
     package{'libffi-devel':
       ensure => 'present',
+      before => Class['mcollective'],
     }
   }
   include mcollective
