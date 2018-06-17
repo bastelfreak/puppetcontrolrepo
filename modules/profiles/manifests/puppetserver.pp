0af5177fafc4e57d2c2afb6c0dc720e8a27f4c45
Tim Meusel <tim@bastelfreak.de>
2018-06-17 11:35:21 +0200

    dont create common modules dir

--- modules/profiles/manifests/puppetserver.pp
+++ modules/profiles/manifests/puppetserver.pp
@@ -10,5 +10,7 @@ class profiles::puppetserver {
     server_reports              => 'puppetdb,foreman',
     server_puppetdb_host        => $facts['fqdn'],
     server_foreman_url          => "https://${facts['fqdn']}",
+    server_common_modules_path  => '', # dont create /etc/puppetlabs/code/environments/common, also purges basemodulepath from /etc/puppetlabs/puppet/puppet.conf
+    server_environments         => undef # dont create /etc/puppetlabs/code/environments/{development,production}
   }
 }
