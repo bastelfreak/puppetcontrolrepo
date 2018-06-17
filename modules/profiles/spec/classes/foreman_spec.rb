3a5c19cdc54efa683de61970f62a88a1d0dc0203
Tim Meusel <tim@bastelfreak.de>
2018-06-17 14:54:54 +0200

    add unit tests for all profiles

---
+++ modules/profiles/spec/classes/foreman_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'profiles::foreman' do
+
+  let :node do
+    'puppet.local'
+  end
+
+  on_supported_os.each do |os, facts|
+    next if facts[:os]['family'] == 'Archlinux'
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
+
+      context 'with all defaults' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_foreman__plugin('column_view') }
+        it { is_expected.to contain_package('rubygem-foreman_maintain-doc') }
+        it { is_expected.to contain_package('rubygem-foreman_maintain') }
+      end
+    end
+  end
+end
