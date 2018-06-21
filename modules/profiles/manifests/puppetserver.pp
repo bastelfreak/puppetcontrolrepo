f3d43badcd362574ed9c188a1e89a9a4a1f01c4c
Tim Meusel <tim@bastelfreak.de>
2018-06-21 22:51:46 +0200

    puppetserver should send metrics to graphite_exporter

--- modules/profiles/manifests/puppetserver.pp
+++ modules/profiles/manifests/puppetserver.pp
@@ -1,17 +1,20 @@
 class profiles::puppetserver {
   # setup puppet master
   class{'puppet':
-    server                      => true,
-    show_diff                   => true,
-    server_puppetserver_jruby9k => true,
-    server_foreman              => true,
-    server_strict_variables     => true,
-    server_storeconfigs_backend => true,
-    server_reports              => 'puppetdb,foreman',
-    server_puppetdb_host        => $facts['fqdn'],
-    server_foreman_url          => "https://${facts['fqdn']}",
-    server_common_modules_path  => '', # dont create /etc/puppetlabs/code/environments/common, also purges basemodulepath from /etc/puppetlabs/puppet/puppet.conf
-    server_environments         => [] # dont create /etc/puppetlabs/code/environments/{development,production}
+    server                         => true,
+    show_diff                      => true,
+    server_puppetserver_jruby9k    => true,
+    server_foreman                 => true,
+    server_strict_variables        => true,
+    server_storeconfigs_backend    => true,
+    server_reports                 => 'puppetdb,foreman',
+    server_puppetdb_host           => $facts['fqdn'],
+    server_foreman_url             => "https://${facts['fqdn']}",
+    server_common_modules_path     => '', # dont create /etc/puppetlabs/code/environments/common, also purges basemodulepath from /etc/puppetlabs/puppet/puppet.conf
+    server_environments            => [] # dont create /etc/puppetlabs/code/environments/{development,production}
+    server_metrics_graphite_enable => true,
+    server_metrics_graphite_host   => '127.0.0.1',
+    server_metrics_graphite_port   => 9109,
   }
 
   include ferm
@@ -36,5 +39,9 @@ class profiles::puppetserver {
     location_deny       => ['all'],
     location_cfg_append => {'access_log' => 'off'},
     ssl                 => false,
+  }
+
+  class{'prometheus::graphite_exporter':
+    options => '-web.listen-address localhost:9108',
   }
 }
