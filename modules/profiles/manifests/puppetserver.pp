8c80a4c55df7a5b442fbb5025e9d6f010a80a2f2
Tim Meusel <tim@bastelfreak.de>
2018-06-21 23:11:52 +0200

    add missing comma

--- modules/profiles/manifests/puppetserver.pp
+++ modules/profiles/manifests/puppetserver.pp
@@ -11,7 +11,7 @@ class profiles::puppetserver {
     server_puppetdb_host           => $facts['fqdn'],
     server_foreman_url             => "https://${facts['fqdn']}",
     server_common_modules_path     => '', # dont create /etc/puppetlabs/code/environments/common, also purges basemodulepath from /etc/puppetlabs/puppet/puppet.conf
-    server_environments            => [] # dont create /etc/puppetlabs/code/environments/{development,production}
+    server_environments            => [], # dont create /etc/puppetlabs/code/environments/{development,production}
     server_metrics_graphite_enable => true,
     server_metrics_graphite_host   => '127.0.0.1',
     server_metrics_graphite_port   => 9109,
