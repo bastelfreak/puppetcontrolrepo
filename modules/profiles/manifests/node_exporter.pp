4592ad6174d7a4780e84e413e59c9740e79be5ce
Tim Meusel <tim@bastelfreak.de>
2018-06-23 12:24:06 +0200

    setup nginx as proxy for node_exporter

--- modules/profiles/manifests/node_exporter.pp
+++ modules/profiles/manifests/node_exporter.pp
@@ -10,7 +10,53 @@ class profiles::node_exporter {
   }
 
   # provide endpoint to monitor node_exporter through ssl
+  case $facts['os']['family'] {
+    'RedHat': {
+      $nginx = 'nginx'
+      $ssl_protocols = 'TLSv1.2'
+    }
+    'Archlinux': {
+      $nginx = 'http'
+      $ssl_protocols = 'TLSv1.2 TLSv1.3'
+    }
+    'Debian': {
+      $nginx = 'www-data'
+      $ssl_protocols = 'TLSv1.2 TLSv1.3'
+    }
+  }
   include nginx
+  file{"/etc/nginx/node_exporter_key_${trusted['certname']}.pem":
+    ensure  => 'file',
+    owner   => $nginx,
+    group   => $nginx,
+    mode    => '0400',
+    source  => "/etc/puppetlabs/puppet/ssl/private_keys/${trusted['certname']}.pem",
+    notify => Nginx::Resource::Server['node_exporter'],
+  }
+  file{"/etc/nginx/node_exporter_cert_${trusted['certname']}.pem":
+    ensure => 'file',
+    owner  => $nginx,
+    group  => $nginx,
+    mode   => '0400',
+    source => "/etc/puppetlabs/puppet/ssl/certs/${trusted['certname']}.pem",
+    notify => Nginx::Resource::Server['node_exporter'],
+  }
+  file{'/etc/nginx/node_exporter_puppet_crl.pem':
+    ensure => 'file',
+    owner  => $nginx,
+    group  => $nginx,
+    mode   => '0400',
+    source => '/etc/puppetlabs/puppet/ssl/crl.pem',
+    notify => Nginx::Resource::Server['node_exporter'],
+  }
+  file{'/etc/nginx/node_exporter_puppet_ca.pem':
+    ensure => 'file',
+    owner  => $nginx,
+    group  => $nginx,
+    mode   => '0400',
+    source => '/etc/puppetlabs/puppet/ssl/certs/ca.pem',
+    notify => Nginx::Resource::Server['node_exporter'],
+  }
 
   # that selboolean allows nginx to talk to tcp port 9100
   if $facts['os']['selinux']['enabled'] {
