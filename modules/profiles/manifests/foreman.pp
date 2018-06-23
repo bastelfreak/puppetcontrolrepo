577aa88cbf807ee14279e93a07d655215388f53f
Tim Meusel <tim@bastelfreak.de>
2018-06-23 13:01:50 +0200

    bind foreman to public interface

--- modules/profiles/manifests/foreman.pp
+++ modules/profiles/manifests/foreman.pp
@@ -2,7 +2,7 @@ class profiles::foreman {
   class{'foreman':
     db_manage           => true,
     configure_epel_repo => false,
-    passenger_interface => $facts['networking']['ip'],
+    passenger_interface => $facts['networking']['primary'],
   }
 
   # run async tasks
