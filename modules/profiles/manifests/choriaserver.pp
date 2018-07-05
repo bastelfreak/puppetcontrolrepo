ddf0b680b37ba3d905467a14b23553eafebf2c8f
Tim Meusel <tim@bastelfreak.de>
2018-07-05 12:58:47 +0200

    purge legacy nats module

--- modules/profiles/manifests/choriaserver.pp
+++ modules/profiles/manifests/choriaserver.pp
@@ -9,14 +9,10 @@ class profiles::choriaserver {
   include make
   include mcollective
   include choria
-  include nats
 
   # mcollective pulls in gems that require make
   Class['make'] -> Class['mcollective']
 
   # mcollective pulls in gems that require gcc
   Class['gcc'] -> Class['mcollective']
-
-  # choria-server doesn't start properly without gnatsd running
-  Class['nats'] -> Class['choria']
 }
