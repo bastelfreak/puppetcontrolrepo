aed9c8dcebdc74e58e4149db04ae5ef8e32235d0
Tim Meusel <tim@bastelfreak.de>
2018-07-05 02:59:22 +0200

    add spec tests for firewall profiles

---
+++ modules/profiles/spec/classes/firewall_https.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'profiles::firewall_https' do
+
+  let :node do
+    'puppet.local'
+  end
+
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
+
+      context 'with all defaults' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_ferm__rule('allow_https') }
+      end
+    end
+  end
+end
