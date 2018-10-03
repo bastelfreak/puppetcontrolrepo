0593264939bb57376251925db09161efd0076003
Tim Meusel <tim@bastelfreak.de>
2018-10-03 22:32:39 +0200

    allow modules to specify their needed dependencies

--- modules/profiles/spec/spec_helper_acceptance.rb
+++ modules/profiles/spec/spec_helper_acceptance.rb
@@ -6,8 +6,19 @@ require 'beaker/module_install_helper'
 run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
 install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
 install_module_on(hosts)
-install_module_dependencies_on(hosts)
 
+# install only needed dependencies. Variable needs to be defined within each spec file
+if $module_dependencies
+  require 'json'
+  metadata = JSON.parse(File.read('metadata.json'))
+  dependencies = metadata['dependencies']
+  filtered_dependencies = dependencies.select{|dependency| $module_dependencies.include?(dependency['name'].split('/')[1])}
+  filtered_dependencies.each do |dependency|
+    install_module_from_forge(dependency['name'], dependency['version_requirement'])
+  end
+else
+  install_module_dependencies_on(hosts)
+end
 RSpec.configure do |c|
   # Readable test descriptions
   c.formatter = :documentation
